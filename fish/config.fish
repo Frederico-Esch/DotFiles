d11f670619323a2450f18e8f47026cef1449b57d
Frederico Esch Pereira <fredeschpe@gmail.com>
2022-11-10 01:48:54 -0300

    FIX

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,22 @@
-/home/frederico/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+ufetch-arco | lolcat
+
+alias lti="leftwm-theme install"
+alias ltu="leftwm-theme uninstall"
+alias lta="leftwm-theme apply"
+alias ltupd="leftwm-theme update"
+alias ltupg="leftwm-theme upgrade"
+
+alias pacman='sudo pacman --color auto'
+alias update='sudo pacman -Syyu'
+
+alias wget="wget -c"
+alias grep='grep --color=auto'
+alias clear='clear && ufetch-arco | lolcat'
+
+alias ll='ls -all'
