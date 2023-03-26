b0da3363977d867ae37e154ae0272c9b8d4b933f
Frederico Esch Pereira <fredeschpe@gmail.com>
2023-03-26 16:13:16 -0300

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set fish_greeting
+set fish_greeting ""
 
-ufetch-arco | lolcat
+set -l val (random 0 1)
+
+if [ $val -eq 0 ]
+    ufetch-arco | lolcat
+else
+    colorscript random
+end
+
 bass source /home/frederico/intel/oneapi/setvars.sh >> /dev/null
 
 alias lti="leftwm-theme install"
@@ -20,3 +27,13 @@ alias wget="wget -c"
 alias grep='grep --color=auto'
 
 alias ll='ls -all'
+
+alias esp_export='. /home/frederico/Downloads/esp-idf/export.fish'
+
+fish_add_path ~/Downloads/Odin/
+fish_add_path ~/.local/bin/
+fish_add_path ~/.cargo/bin/
+fish_add_path ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/
+setxkbmap -option compose:menu
+#fish_add_path ~/Downloads/ols/
+
