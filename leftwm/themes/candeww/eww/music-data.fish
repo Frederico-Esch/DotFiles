640452a82fd1a41646761a2760de6ee336194be2
FredEsch <fredeschpe@gmail.com>
2024-05-22 09:03:41 -0300

    fix bug where closing spotify isn't detected because awk doesn't print '\n', just add the raw print before and it should fix the issue, there are better approaches, I don't care to think of one though

--- leftwm/themes/candeww/eww/music-data.fish
+++ leftwm/themes/candeww/eww/music-data.fish
@@ -1,4 +1,5 @@
 #!/bin/fish
 playerctl -p 'spotify' --follow metadata --format $(echo -n -e '{{artist}}\t{{title}}\t{{status}}\t{{position}}\t{{mpris:length}}\t{{mpris:artUrl}}\t{{duration(position)}}\t{{loop}}\t{{shuffle}}') | while read -l f
+    echo $f
     echo -n $f | awk -F\t '{gsub(/"/, "\\\\\\""); print("{\\"display\\": \\""$1" - "$2"\\", \\"artist\\": \\""$1"\\", \\"title\\": \\""$2"\\", \\"status\\": \\""$3"\\", \\"position\\": "$4", \\"length\\": "$5", \\"cover\\": \\""$6"\\", \\"duration\\": \\""$7"\\", \\"repeat\\": \\""$8"\\", \\"shuffle\\": \\""$9"\\" }")}'
 end
